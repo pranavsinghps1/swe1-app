fbb1612c380c9339aba0cf5221d36f7b1b09d1d2
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-02-23 04:04:28 -0500

    env changes4

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mb^$41$1@_+*wwfaqiapg9*fe0$8hn&sadv6%tx6bh34!5^#x&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

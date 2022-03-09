0374a1fe4f7d3303c6d1920a4031c2f4cafeb35b
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-03-09 13:41:25 -0500

    reqs change

--- polls/tests.py
+++ polls/tests.py
@@ -7,12 +7,6 @@ from django.utils import timezone
 
 from .models import Question
 
-
-def create_question(question_text, days):
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
-
-
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         response = self.client.get(reverse("polls:index"))

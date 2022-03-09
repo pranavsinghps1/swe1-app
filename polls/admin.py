204106ed42add76dc1f1580d1dfd21dc00c0daed
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-03-09 12:38:29 -0500

    Travis and Flake8 integration

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -9,10 +10,11 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ("question_text", "pub_date", "was_published_recently")
+
 
 admin.site.register(Question, QuestionAdmin)
